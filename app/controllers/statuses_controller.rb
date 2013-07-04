f2f42c2963e53d82ffb5d192394e3df9c5301e65
Tope Alabi <alabitemitope@gmail.com>
2013-07-03 20:04:23 -0400

    added tests for statuses controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,7 @@
 class StatusesController < ApplicationController
   # GET /statuses
   # GET /statuses.json
-  before_filter :authenticate_user!, only: [:new]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @statuses = Status.all
 
